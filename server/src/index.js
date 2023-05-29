4817957d9050f16c3938357ffe32a05326ffa69b
Franco Quevedo <francoq96@gmail.com>
2023-05-29 19:01:26 -0300

    Refactorizado de la conexión a la db

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,27 @@
-import * as dotenv from "dotenv";
+import * as dotenv from "dotenv"
 // Toma las variables configuradas por ENV dentro del docker-compose / dockerfile
-// dotenv.config({ path: './.env'});
-dotenv.config();
+// dotenv.config({ path: './.env'})
+dotenv.config()
 
-import expressServer from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import cors from "cors";
-import Sequelize from "sequelize";
+import expressServer from "express"
+import bodyParser from "body-parser"
+import morgan from "morgan"
+import cors from "cors"
 
-import controlRouter from "./routes/control.routes.js";
+import { dbConnection } from "./db/dbconfig.js"
 
-const app = expressServer();
-const PORT = process.env.SERVER_PORT || 3000;
+import controlRouter from "./routes/control.routes.js"
+
+const app = expressServer()
+const PORT = process.env.SERVER_PORT || 3000
 
 //Necesitamos body-parser para formatear los post en express
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.use("/api/control", controlRouter);
+app.use("/api/control", controlRouter)
 
 // app.use("/public-key", "id_rsa.pub") // formato x.509
 
@@ -28,18 +29,7 @@ app.use("/api/control", controlRouter);
 
 // Levantamos el servidor para que escuche peticiones
 app.listen(PORT, () => {
-  console.log(`Server on container port ${PORT}`);
-  console.log(`Server on localhost port ${process.env.LOCALHOST_PORT}`);
-  console.log("----------------------");
-});
+  console.log(`LabRem Control - Server on ${process.env.LOCALHOST_PORT}:${PORT}`)
+})
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
-  }
-);
+export const db = dbConnection
