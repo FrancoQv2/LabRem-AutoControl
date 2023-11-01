1c07bbe7f6398423995f3f26dcadc482c1aa9340
Franco Quevedo <fquevedo@onebittech.com>
2023-11-01 16:17:27 -0300

    feat: agregado control para arduino

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,4 @@
 import * as dotenv from "dotenv"
-// Toma las variables configuradas por ENV dentro del docker-compose / dockerfile
-// dotenv.config({ path: './.env'})
 dotenv.config()
 
 import expressServer from "express"
@@ -27,15 +25,12 @@ const options = {
 const app = expressServer()
 const PORT = 3000
 
-//Necesitamos body-parser para formatear los post en express
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.use("/api/control", controlRouter)
-
-// app.use("/public-key", "id_rsa.pub") // formato x.509
 
 // ---------------------------------------------------------------
 
---
+++ server/src/utils/arduino.js
@@ -0,0 +1,41 @@
+import axios from "axios"
+import { delay } from "./delay.js"
+
+const URL_ARDUINO = process.env.URL_ARDUINO
+
+const GET  = 'GET /  HTTP/1.1'
+const POST = 'POST / HTTP/1.1'
+
+// curl -X GET http://10.0.255.11x -H 'Content-Type: text/plain' -d 'GET /  HTTP/1.1'
+async function arduinoGET() {
+    // const curlGET = `curl -X GET ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${GET} ${body}'`
+    const curlGET = `curl -X GET ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${GET}'`
+
+    return new Promise((resolve, reject) => {
+        exec(curlGET, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+                return
+            }
+            resolve(stdout)
+        })
+    })
+}
+
+// curl -X POST http://10.0.255.11x -H 'Content-Type: text/plain' -d 'POST / HTTP/1.1 {"Estado": [3,true,true],"Analogico": ["central",40,40]}'
+async function arduinoPOST(diafragma, distanciaLente, distanciaPantalla) {
+    const body = `{"Estado": [3,true,true],"Analogico": [${diafragma},${distanciaLente},${distanciaPantalla}]}`
+    const curlPOST = `curl -X POST ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${POST} ${body}'`
+
+    return new Promise((resolve, reject) => {
+        exec(curlPOST, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+                return
+            }
+            resolve(stdout)
+        })
+    })
+}
+
+export { arduinoGET, arduinoPOST }
